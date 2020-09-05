42cb4e3756eb5fe776b2e25162d816d85130f2fd
nickgarfield <ntgarfield@gmail.com>
2020-09-05 09:10:57 -0500

    📦 NEW: Hello world web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+    })
+    .bind("localhost:3000")?
+    .run()
+    .await
+}
+
+#[get("/")]
+pub async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
